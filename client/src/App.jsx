00c60b5ba3b3b86f1c63ddc957d31ca8b0541d56
Leman Gahramanova <gahramanovalamann@gmail.com>
2025-07-08 23:52:18 +0400

    Project setup and installing dependencies Server side code setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="">Click me</Button>
+      <div className="bg-red-600 flex">
+      <p className="bg-red-500">sa</p>
+      <p>sa</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
