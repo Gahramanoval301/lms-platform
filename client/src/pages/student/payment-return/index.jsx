4b4ff84be152ef8ee83fa52c1dda9193552445b0
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:30:24 +0400

    add payment-success return page

---
+++ client/src/pages/student/payment-return/index.jsx
@@ -0,0 +1,44 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { captureFinalizePaymentService } from "@/services";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const PaypalPaymentReturnPage = () => {
+  // This page is used to display a message to the user while the payment is being processed.
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const paymentId = params.get("paymentId");
+  const payerId = params.get("PayerID");
+
+  useEffect(() => {
+    if (paymentId && payerId) {
+      async function capturePayment() {
+        const currentOrderId = JSON.parse(
+          sessionStorage.getItem("currentOrderId")
+        );
+
+        const response = await captureFinalizePaymentService(
+          paymentId,
+          payerId,
+          currentOrderId
+        );
+
+        if (response.success) {
+          sessionStorage.removeItem("currentOrderId");
+          window.location.href = "/student-courses";
+        }
+      }
+      capturePayment();
+    }
+  }, [paymentId, payerId]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Processing payment... Please Wait...</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default PaypalPaymentReturnPage;
