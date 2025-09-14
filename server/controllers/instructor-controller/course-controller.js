fac12a060b6f2a0fece9da9cbc3149adc7fe0550
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:30:46 +0400

    instructor controller

--- server/controllers/instructor-controller/course-controller.js
+++ server/controllers/instructor-controller/course-controller.js
@@ -1,92 +1,91 @@
-const Course = require('../../models/Course');
-
+const Course = require("../../models/Course");
 
 const addNewCourse = async (req, res) => {
-    try {
-        const courseData  = req.body;
-        const newlyCreatedCourse = new Course(courseData);
-        const saveCourse = await newlyCreatedCourse.save();
+  try {
+    const courseData = req.body;
+    const newlyCreatedCourse = new Course(courseData);
+    const saveCourse = await newlyCreatedCourse.save();
 
-        if(saveCourse){
- res.status(201).json({
-            success: true,
-            message: "New Course Added",
-            data: saveCourse
-        })
-        }
-
-       
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success:false, message: "Internal Server Error" });
+    if (saveCourse) {
+      res.status(201).json({
+        success: true,
+        message: "New Course Added",
+        data: saveCourse,
+      });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 const getAllCourses = async (req, res) => {
-    try {
-        const coursesList = await Course.find({});
+  try {
+    const coursesList = await Course.find({});
 
-        res.status(200).json({
-            success: true,
-            data:coursesList
-        })
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success:false, message: "Internal Server Error" });
-    }
-}
+    res.status(200).json({
+      success: true,
+      data: coursesList,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 const getCourseDetailsById = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const courseDetails = await Course.findById(id);
+  try {
+    const { id } = req.params;
+    const courseDetails = await Course.findById(id);
 
-        if(!courseDetails){
-            return res.status(404).json({
-                success:false, 
-                message: "Course not found"
-            })
-        }
-
-        res.status(200).json({
-            success:true, 
-            data:courseDetails
-
-        })
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success:false, message: "Internal Server Error" });
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
     }
-}
+
+    res.status(200).json({
+      success: true,
+      data: courseDetails,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 const updateCourseById = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const updatedCourseData = req.body;
-        const updatedCourse = await Course.findByIdAndUpdate(id, updatedCourseData, {new:true});
+  try {
+    const { id } = req.params;
+    const updatedCourseData = req.body;
+    const updatedCourse = await Course.findByIdAndUpdate(
+      id,
+      updatedCourseData,
+      { new: true }
+    );
 
-        if(!updatedCourse){
-            return res.status(404).json({
-                success:false, 
-                message: "Course not found"
-            })
-        }
-
-        res.status(200).json({
-            success:true, 
-            message: "Course Updated",
-            data:updatedCourse
-        })
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ success:false, message: "Internal Server Error" });
+    if (!updatedCourse) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
     }
-}
 
-module.exports = {addNewCourse, getAllCourses, getCourseDetailsById, updateCourseById}
+    res.status(200).json({
+      success: true,
+      message: "Course Updated",
+      data: updatedCourse,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  addNewCourse,
+  getAllCourses,
+  getCourseDetailsById,
+  updateCourseById,
+};
