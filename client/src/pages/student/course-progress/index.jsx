943b9ecb96b5f5b90a12d5ae5a276b0c22eb6857
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:33:37 +0400

    add course-progress page and fix all its bugs

---
+++ client/src/pages/student/course-progress/index.jsx
@@ -0,0 +1,112 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { AuthContext } from "@/context/auth-context";
+import { StudentContext } from "@/context/student-context";
+import { getCurrentCourseProgressService } from "@/services";
+import { ChevronLeft } from "lucide-react";
+import Confetti from "react-confetti";
+
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+
+const StudentViewCourseProgressPage = () => {
+  const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+  const { studentCurrentCourseProgress, setStudentCurrentCourseProgresss } =
+    useContext(StudentContext);
+  const [lockCourse, setLockCourse] = useState(false);
+  const [currentLectures, setCurrentLecture] = useState(null);
+  const [showCourseCompleteDialog, , setShowCourseCompleteDialog] =
+    useState(false);
+  const [showConfetti, , setShowConfetti] = useState(false);
+
+  const { id } = useParams();
+
+  async function fetchCurrentCourseProgress() {
+    const response = await getCurrentCourseProgressService(auth?.user?._id, id);
+    console.log(response);
+
+    if (response?.success) {
+      if (!response?.data?.isPurchased) {
+        setLockCourse(true);
+      } else {
+        setStudentCurrentCourseProgresss({
+          courseDetails: response?.data?.courseDetails,
+          progress: response?.data?.progress,
+        });
+
+        if (response?.data?.completed) {
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+          setShowCourseCompleteDialog(true);
+          setShowConfetti(true);
+
+          return;
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrentCourseProgress();
+  }, [id]);
+
+  useEffect(() => {
+    if (showConfetti)
+      setTimeout(() => {
+        setShowConfetti(false);
+      }, 5000);
+  }, [showConfetti]);
+
+  return (
+    <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
+      {showConfetti && <Confetti />}
+      <div className="flex items-center justify-between p-4 bg-[#1c1d1f] border-b border-gray-700">
+        <div className="flex items-center space-x-4">
+          <Button
+            onClick={() => navigate("/student-courses")}
+            className={"text-black"}
+            variant={"ghost"}
+            size={"sm"}
+          >
+            <ChevronLeft className="h-4 w-4 mr-2" />
+            Back to My Courses Page
+          </Button>
+          <h1></h1>
+        </div>
+      </div>
+      <Dialog open={lockCourse}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>You can't view this page</DialogTitle>
+            <DialogDescription>
+              Please purchase this course to get access
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showCourseCompleteDialog}>
+        <DialogContent className="sm:w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Congrulations!</DialogTitle>
+            <DialogDescription className="flex flex-col gap-3">
+              <Label>You have completed the course.</Label>
+              <div className="flex flex-row gap-3">
+                <Button>My Courses Page</Button>
+                <Button>Rewatch Course</Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default StudentViewCourseProgressPage;
