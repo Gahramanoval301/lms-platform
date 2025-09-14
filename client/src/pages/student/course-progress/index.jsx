7f6bf62b2cb2f14fe0aeb60738614b4773b17b41
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:52:42 +0400

    add new logics for course-progress page for full functionality

--- client/src/pages/student/course-progress/index.jsx
+++ client/src/pages/student/course-progress/index.jsx
@@ -9,47 +9,92 @@ import {
 import { Label } from "@/components/ui/label";
 import { AuthContext } from "@/context/auth-context";
 import { StudentContext } from "@/context/student-context";
-import { getCurrentCourseProgressService } from "@/services";
-import { ChevronLeft } from "lucide-react";
+import {
+  getCurrentCourseProgressService,
+  markLectureAsViewedService,
+  resetCourseProgressService,
+} from "@/services";
+import { Check, ChevronLeft, ChevronRight, Play } from "lucide-react";
 import Confetti from "react-confetti";
 
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
+import VideoPlayer from "@/components/video-player";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const StudentViewCourseProgressPage = () => {
   const navigate = useNavigate();
   const { auth } = useContext(AuthContext);
-  const { studentCurrentCourseProgress, setStudentCurrentCourseProgresss } =
+  const { studentCurrentCourseProgress, setStudentCurrentCourseProgress } =
     useContext(StudentContext);
   const [lockCourse, setLockCourse] = useState(false);
-  const [currentLectures, setCurrentLecture] = useState(null);
-  const [showCourseCompleteDialog, , setShowCourseCompleteDialog] =
+  const [currentLecture, setCurrentLecture] = useState(null);
+  const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
     useState(false);
-  const [showConfetti, , setShowConfetti] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const { id } = useParams();
 
   async function fetchCurrentCourseProgress() {
     const response = await getCurrentCourseProgressService(auth?.user?._id, id);
-    console.log(response);
 
     if (response?.success) {
       if (!response?.data?.isPurchased) {
         setLockCourse(true);
       } else {
-        setStudentCurrentCourseProgresss({
+        setStudentCurrentCourseProgress({
           courseDetails: response?.data?.courseDetails,
           progress: response?.data?.progress,
         });
 
         if (response?.data?.completed) {
-          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
-          setShowCourseCompleteDialog(true);
           setShowConfetti(true);
+          setShowCourseCompleteDialog(true);
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
 
           return;
         }
+
+        if (response?.data?.progress?.length == 0) {
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+        } else {
+          const lastIndexOfViewedAsTrue = response?.data?.progress.reduceRight(
+            (acc, obj, index) => {
+              return acc === -1 && obj.viewed ? index : acc;
+            },
+            -1
+          );
+
+          setCurrentLecture(
+            response?.data?.courseDetails?.curriculum[
+              lastIndexOfViewedAsTrue + 1
+            ]
+          );
+        }
       }
+    }
+  }
+
+  const handleReachEndOfVideo = () => {
+    setCurrentLecture({
+      ...currentLecture,
+      progressValue: 1,
+    });
+  };
+
+  async function handleRewatchCourse() {
+    const response = await resetCourseProgressService(
+      auth?.user?._id,
+      studentCurrentCourseProgress?.courseDetails?._id
+    );
+
+    if (response?.success) {
+      setCurrentLecture(null);
+      setShowConfetti(false);
+      setShowCourseCompleteDialog(false);
+      fetchCurrentCourseProgress();
     }
   }
 
@@ -57,11 +102,33 @@ const StudentViewCourseProgressPage = () => {
     fetchCurrentCourseProgress();
   }, [id]);
 
+  async function updateCourseProgress() {
+    if (currentLecture) {
+      console.log();
+
+      const response = await markLectureAsViewedService(
+        auth?.user?._id,
+        studentCurrentCourseProgress?.courseDetails?._id,
+        currentLecture?._id
+      );
+
+      if (response?.success) {
+        fetchCurrentCourseProgress();
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (currentLecture?.progressValue === 1) {
+      updateCourseProgress();
+    }
+  }, [currentLecture]);
+
   useEffect(() => {
     if (showConfetti)
       setTimeout(() => {
         setShowConfetti(false);
-      }, 5000);
+      }, 8000);
   }, [showConfetti]);
 
   return (
@@ -78,11 +145,97 @@ const StudentViewCourseProgressPage = () => {
             <ChevronLeft className="h-4 w-4 mr-2" />
             Back to My Courses Page
           </Button>
-          <h1></h1>
+          <h1 className="text-lg font-bold hidden md:block">
+            {studentCurrentCourseProgress?.courseDetails?.title}
+          </h1>
+        </div>
+        <Button onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+          {isSidebarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+      <div className="flex flex-1 overflow-hidden ">
+        <div
+          className={`flex-1 ${
+            isSidebarOpen ? "mr-[400px]" : ""
+          } transition-all duration-300`}
+        >
+          {/* handle reach the end of video we need to catch it function -handleReachEndOfVideo */}
+          <VideoPlayer
+            videoContainerClassname="w-full h-full"
+            url={currentLecture?.videoUrl}
+            handleVideoEnded={handleReachEndOfVideo}
+          />
+          <div className="p-6 bg-[#1c1d1f] ">
+            <h2 className="text-2xl font-bold mb-2">{currentLecture?.title}</h2>
+          </div>
+        </div>
+        <div
+          className={`fixed top-[64px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-1 border-gray-700 transition-all duration-300 ${
+            isSidebarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <Tabs defaultValue="content" className={"h-full flex flex-col"}>
+            <TabsList
+              className={"bg-[#1c1d1f] grid w-full grid-cols-2 p-0 h-14 "}
+            >
+              <TabsTrigger
+                value="content"
+                className={" text-black rounded-none h-full"}
+              >
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger
+                value="overview"
+                className={" text-black rounded-none h-full"}
+              >
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className={"h-full "}>
+                <div className="p-4 space-y-4">
+                  {studentCurrentCourseProgress?.courseDetails?.curriculum.map(
+                    (item) => (
+                      <div
+                        className="flex items-center space-x-2 text-sm text-white font-bold cursor-pointer"
+                        key={item?.id}
+                      >
+                        {studentCurrentCourseProgress?.progress?.find(
+                          (progressItem) => {
+                            console.log(progressItem, item._id, "lemannnn!!");
+                            return progressItem?.lectureId === item?._id;
+                          }
+                        )?.viewed ? (
+                          <Check className="h-4 w-4 text-green-800" />
+                        ) : (
+                          <Play className="h-4 w-4 " />
+                        )}
+                        <span>{item?.title}</span>
+                      </div>
+                    )
+                  )}
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className={"flex-1 overflow-hidden"}>
+              <ScrollArea className={"h-full "}>
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About this course</h2>
+                  <p className="text-gray-400">
+                    {studentCurrentCourseProgress?.courseDetails?.description}
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
       <Dialog open={lockCourse}>
-        <DialogContent className="sm:w-[425px]">
+        <DialogContent showOverlay={false} className="sm:w-[425px]">
           <DialogHeader>
             <DialogTitle>You can't view this page</DialogTitle>
             <DialogDescription>
@@ -98,8 +251,10 @@ const StudentViewCourseProgressPage = () => {
             <DialogDescription className="flex flex-col gap-3">
               <Label>You have completed the course.</Label>
               <div className="flex flex-row gap-3">
-                <Button>My Courses Page</Button>
-                <Button>Rewatch Course</Button>
+                <Button onClick={() => navigate("/student-courses")}>
+                  My Courses Page
+                </Button>
+                <Button onClick={handleRewatchCourse}>Rewatch Course</Button>
               </div>
             </DialogDescription>
           </DialogHeader>
