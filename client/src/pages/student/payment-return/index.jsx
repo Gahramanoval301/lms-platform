06cf7b074f9ce21eb0c38412b0bf9a1b6db73da4
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:51:39 +0400

    prevent second click on buy now btn

--- client/src/pages/student/payment-return/index.jsx
+++ client/src/pages/student/payment-return/index.jsx
@@ -1,6 +1,6 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { captureFinalizePaymentService } from "@/services";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router";
 
 const PaypalPaymentReturnPage = () => {
@@ -9,9 +9,11 @@ const PaypalPaymentReturnPage = () => {
   const params = new URLSearchParams(location.search);
   const paymentId = params.get("paymentId");
   const payerId = params.get("PayerID");
+  const hasCaptured = useRef(false);
 
   useEffect(() => {
-    if (paymentId && payerId) {
+    if (paymentId && payerId && !hasCaptured.current) {
+      hasCaptured.current = true;
       async function capturePayment() {
         const currentOrderId = JSON.parse(
           sessionStorage.getItem("currentOrderId")
