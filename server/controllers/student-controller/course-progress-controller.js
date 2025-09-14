d6c92447ec2e2a801a3515ce456779b1b8b1d534
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-14 21:56:00 +0400

    add new APIs for the course-progress page, and fix bugs there

--- server/controllers/student-controller/course-progress-controller.js
+++ server/controllers/student-controller/course-progress-controller.js
@@ -5,6 +5,70 @@ const StudentCourses = require("../../models/StudentCourses");
 // mark current lecture as viewed
 const markCurrentLectureAsViewed = async (req, res) => {
   try {
+    const { userId, courseId, lectureId } = req.body;
+
+    let progress = await CourseProgress.findOne({ userId, courseId });
+
+    if (!progress) {
+      progress = new CourseProgress({
+        userId,
+        courseId,
+        lecturesProgress: [{ lectureId, viewed: true, dateViewed: new Date() }],
+      });
+
+      await progress.save();
+    } else {
+      const lecturesProgress = progress.lecturesProgress.find(
+        (item) => item.lectureId.toString() === lectureId.toString()
+      );
+
+      if (!lecturesProgress) {
+        console.log("sagolll!");
+
+        console.log(lectureId, "lecureid");
+        progress.lecturesProgress.push({
+          lectureId,
+          viewed: true,
+          dateViewed: new Date(),
+        });
+      } else {
+        console.log("salammmm");
+        // i don;t know
+
+        // progress.lecturesProgress.push({
+        //   lectureId,
+        //   viewed: true,
+        //   dateViewed: new Date(),
+        // });
+      }
+      await progress.save();
+    }
+
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return res
+        .status(404)
+        .json({ message: "Course not found", success: false });
+    }
+
+    //check all the lectures are viewed or not
+    const allLecturesAreViewed =
+      progress.lecturesProgress.length === course.curriculum.length &&
+      progress.lecturesProgress.every((item) => item.viewed);
+
+    if (allLecturesAreViewed) {
+      progress.completed = true;
+      progress.completionDate = new Date();
+
+      await progress.save();
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Lecture marked as viewed successfully",
+      data: progress,
+    });
   } catch (error) {
     console.log("in function getCurrentCourseProgress", error);
     res.status(500).json({ message: "Internal Server Error", success: false });
@@ -18,17 +82,19 @@ const getCurrentCourseProgress = async (req, res) => {
 
     const studentPurchasedCourses = await StudentCourses.findOne({ userId });
 
-    const isCurrentCoursePurchaseByCurrentUserOrNot =
-      studentPurchasedCourses?.courses?.findIndex(
-        (item) => item.courseId == courseId
-      );
+    // console.log(studentPurchasedCourses, "studentPurchasedCourses");
 
-    if (!isCurrentCoursePurchaseByCurrentUserOrNot) {
+    const isCurrentCoursePurchaseByCurrentUserOrNot =
+      studentPurchasedCourses?.courses?.findIndex((item) => {
+        return item.courseId == courseId;
+      });
+
+    if (isCurrentCoursePurchaseByCurrentUserOrNot === -1) {
       return res.status(200).json({
         message: "Course not purchased by user",
         success: true,
         data: {
-          isPurchased: true,
+          isPurchased: false,
         },
       });
     }
@@ -82,6 +148,26 @@ const getCurrentCourseProgress = async (req, res) => {
 //reset course progress
 const resetCurrentCourseProgress = async (req, res) => {
   try {
+    const { userId, courseId } = req.body;
+    const progress = await CourseProgress.findOne({ userId, courseId });
+
+    if (!progress) {
+      return res
+        .status(404)
+        .json({ message: "Progress not found", success: false });
+    }
+
+    progress.lecturesProgress = [];
+    progress.completed = false;
+    progress.completionDate = null;
+
+    await progress.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Progress reset successfully",
+      data: progress,
+    });
   } catch (error) {
     console.log("in function getCurrentCourseProgress", error);
     res.status(500).json({ message: "Internal Server Error", success: false });
