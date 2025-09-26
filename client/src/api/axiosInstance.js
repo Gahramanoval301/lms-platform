6633e8a1231088bf03f8ea0484975e6beb90a288
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-26 21:22:04 +0400

    add credentials true

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,15 +2,42 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5001",
+  withCredentials: true,
 });
+
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     console.log(error.response, originalRequest._retry, "responseee");
+
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+
+//       try {
+//         // Call backend to refresh tokens (cookies are auto-sent)
+//         await axiosInstance.post("/auth/refresh-token");
+
+//         // Retry the original request
+//         return axiosInstance(originalRequest);
+//       } catch (refreshError) {
+//         // Refresh failed → log out
+//         console.error("Refresh failed", refreshError);
+//         return Promise.reject(refreshError);
+//       }
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
+    // const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
 
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
+    // if (accessToken) {
+    //   config.headers.Authorization = `Bearer ${accessToken}`;
+    // }
 
     return config;
   },
